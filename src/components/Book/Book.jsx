184c1a4238e16d59e70e095259f37b1335e0fa0e
Khurshedspi <khurshed5322@gmail.com>
2024-11-01 07:42:33 +0600

    added cards

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,22 +1,43 @@
+import { Link } from "react-router-dom";
+
 const Book = ({ book }) => {
-  const { image, bookName, author } = book;
+  const { bookId, image, bookName, author, tags, category, rating } = book;
   return (
-    <div className="card bg-base-100 w-96 shadow-xl p-6">
-      <figure className="bg-gray-200 py-8 rounded-2xl">
-        <img className="h-[166px]" src={image} alt={bookName} />
-      </figure>
-      <div className="card-body">
-        <h2 className="card-title">
-          {bookName}
-          <div className="badge badge-secondary">NEW</div>
-        </h2>
-        <p>{author}</p>
-        <div className="card-actions justify-end">
-          <div className="badge badge-outline">Fashion</div>
-          <div className="badge badge-outline">Products</div>
+    <Link to={`/books/${bookId}`}>
+      <div className="card bg-base-100 w-96 shadow-xl p-6">
+        <figure className="bg-gray-200 py-8 rounded-2xl">
+          <img className="h-[166px]" src={image} alt={bookName} />
+        </figure>
+        <div className="card-body">
+          <div className="flex justify-center gap-4">
+            {tags.map((tag, idx) => (
+              <button
+                key={idx}
+                className="btn btn-xs bg-green-200 text-green-500"
+              >
+                {tag}
+              </button>
+            ))}
+          </div>
+          <h2 className="card-title">{bookName}</h2>
+          <p>{author}</p>
+          <div className="border-t-2 border-dashed"></div>
+          <div className="card-actions justify-between">
+            <div className="badge badge-outline">{category}</div>
+            <div className="badge badge-outline">
+              <div className="rating rating-sm">
+                {rating}
+                <input
+                  type="radio"
+                  name="rating-6"
+                  className="mask mask-star-2 bg-orange-400 ml-2"
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 };
 
