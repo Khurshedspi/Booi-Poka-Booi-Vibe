4321cb01c59dcf3469a45ae9ab2bc6f06735dc61
Khurshedspi <khurshed5322@gmail.com>
2024-11-01 09:39:07 +0600

    mark and wish list added local storage

--- src/components/BookDetail/BookDetail.jsx
+++ src/components/BookDetail/BookDetail.jsx
@@ -1,12 +1,39 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, useParams } from "react-router-dom";
+import { addToStoredReadList, addToStoredWishList } from "../../utility/addToDb";
 
 
 const BookDetail = () => {
     const {bookId} = useParams();
-    console.log(bookId);
+    const id = parseInt(bookId)
+    const data = useLoaderData();
+    const book = data.find(book => book.bookId === id)
+    const {bookId: currentBookId, image} = book;
+
+    // mrS1
+    const handleMarkAsRead = (id) =>{
+        /**
+         * 1. understand what to store or save => bookId
+         * 2. where to store: database
+         * 3. array, list, collection:
+         * 4. Check: if the book is already in the readList
+         * 5. If not, then add the book to the list
+         * 6. if yes, do not  add the book
+         */
+
+        addToStoredReadList(id);
+
+    }
+
+    const handleAddToWishList = (id) => {
+        addToStoredWishList(id)
+    }
     return (
-        <div>
-            <h2>book detail</h2>
+        <div className="my-12">
+            <h2>Book Details: {bookId}</h2>
+            <img className="w-36 " src={image} alt="" />
+            <br />
+            <button onClick={() =>handleMarkAsRead(bookId)} className="btn btn-outline mr-4 btn-accent">Mark As Read</button>
+            <button onClick={()=> handleAddToWishList(bookId)} className="btn btn-accent">Add To Wishlist</button>
         </div>
     );
 };
---
+++ src/utility/addToDb.js
@@ -0,0 +1,59 @@
+
+
+const getStoredReadList = () =>{
+    //read-list
+    const storedListStr = localStorage.getItem('read-list');
+    if(storedListStr){
+        const storedList =JSON.parse(storedListStr);
+        return storedList;
+    }
+    else{
+        return [];
+    }
+
+}
+
+const addToStoredReadList = (id) => {
+    const storedList = getStoredReadList();
+    if(storedList.includes(id)){
+        //already exists. Do not add it
+        console.log(id, 'already exists in the read list');
+
+    }
+    else{
+        storedList.push(id);
+        // data store in LS 
+        const storedListStr = JSON.stringify(storedList);
+        localStorage.setItem('read-list', storedListStr);
+
+    }
+}
+
+// for wish list 
+
+const getStoredWishList = () => {
+    //wish-list
+    const storedWisListStr = localStorage.getItem('wish-list');
+    if(storedWisListStr){
+        const storedWishList = JSON.parse(storedWisListStr);
+        return storedWishList;
+    }
+    return [];
+}
+
+const addToStoredWishList = (id) => {
+    const storedWishList = getStoredWishList();
+    if(storedWishList.includes(id)){
+        //already exists, Do no add it
+        console.log(id, 'already exists in the read list');
+    }
+    else{
+        storedWishList.push(id);
+        // wish list store LS 
+        const storedWisListStr = JSON.stringify(storedWishList);
+        localStorage.setItem('wish-list', storedWisListStr)
+    }
+}
+
+
+export {addToStoredReadList, addToStoredWishList}
